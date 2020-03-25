b60b10d86d7673743362d7bfafc7b1dff4c8cf35
Alex Lementuev <a.lementuev@gmail.com>
2020-03-25 17:28:15 -0400

    Added a-b pruning

--- project_2/minimax.py
+++ project_2/minimax.py
@@ -1,3 +1,6 @@
+from math import inf
+
+
 class Minimax:
     def __init__(self, state, terminal_test, moves_func, eval_func):
         self._state = state
@@ -6,20 +9,38 @@ class Minimax:
         self._eval_func = eval_func
 
     def decision(self):
-        move, utility = self._solve(self._state, maximize=True)
+        move, utility = self._solve(self._state, maximize=True, alpha=-inf, beta=inf)
         return move, utility
 
-    def _solve(self, state, maximize):
+    def _solve(self, state, maximize, alpha, beta):
         if self._terminal_test(state):
             return state, self._eval_func(state)
 
         best_move = None
-        best_utility = float('-inf') if maximize else float('+inf')
         moves = self._moves_func(state)
-        for move in moves:
-            _, utility = self._solve(move, not maximize)
-            if maximize and utility > best_utility or not maximize and utility < best_utility:
-                best_utility = utility
-                best_move = move
+        if maximize:
+            best_utility = -inf
+            for move in moves:
+                _, utility = self._solve(move, False, alpha, beta)
+                if utility > best_utility:
+                    best_utility = utility
+                    best_move = move
+
+                if utility > beta:
+                    break
+
+                alpha = max(alpha, utility)
+        else:
+            best_utility = inf
+            for move in moves:
+                _, utility = self._solve(move, True, alpha, beta)
+                if utility < best_utility:
+                    best_utility = utility
+                    best_move = move
+
+                if utility < alpha:
+                    break
+
+                beta = min(beta, utility)
 
         return best_move, best_utility
--- project_2/minimax_test.py
+++ project_2/minimax_test.py
@@ -51,22 +51,25 @@ def terminal_test(node):
     return node.is_leaf()
 
 
-def moves_func(node):
+def moves(node):
     return node.children
 
 
-def eval_func(node):
+def eval(node):
     return node.val
 
 
-def create_minimax(node):
-    return Minimax(state=node, terminal_test=terminal_test, moves_func=moves_func, eval_func=eval_func)
+def create_minimax(node, terminal_test_func=None, moves_func=None, eval_func=None):
+    return Minimax(state=node,
+                   terminal_test=terminal_test_func or terminal_test,
+                   moves_func=moves_func or moves,
+                   eval_func=eval_func or eval)
 
 
 class Test(TestCase):
     def test_decision1(self):
-        self._test_decision(child_index=1, utility=2, values=[1, 2], branching=2)
-        self._test_decision(child_index=0, utility=2, values=[2, 1], branching=2)
+        self._test_decision(child_index=1, utility=2, values=[1, 2], branch_factor=2)
+        self._test_decision(child_index=0, utility=2, values=[2, 1], branch_factor=2)
 
     def test_decision2(self):
         """
@@ -74,27 +77,27 @@ class Test(TestCase):
          1   3
         1 2 3 4
         """
-        self._test_decision(child_index=1, utility=3, values=[1, 2, 3, 4], branching=2)
+        self._test_decision(child_index=1, utility=3, values=[1, 2, 3, 4], branch_factor=2)
 
         """
            2
          2   1
         4 2 3 1 
         """
-        self._test_decision(child_index=0, utility=2, values=[4, 2, 3, 1], branching=2)
+        self._test_decision(child_index=0, utility=2, values=[4, 2, 3, 1], branch_factor=2)
 
     def test_decision3(self):
         """
            4
         1 2 3 4
         """
-        self._test_decision(child_index=3, utility=4, values=[1, 2, 3, 4], branching=4)
+        self._test_decision(child_index=3, utility=4, values=[1, 2, 3, 4], branch_factor=4)
 
         """
            4
         1 4 2 3
         """
-        self._test_decision(child_index=1, utility=4, values=[1, 4, 2, 3], branching=4)
+        self._test_decision(child_index=1, utility=4, values=[1, 4, 2, 3], branch_factor=4)
 
     def test_decision4(self):
         """
@@ -103,7 +106,7 @@ class Test(TestCase):
          2    4    6    8      max
         1 2  3 4  5 6  7 8     min
         """
-        self._test_decision(child_index=1, utility=6, values=[1, 2, 3, 4, 5, 6, 7, 8], branching=2)
+        self._test_decision(child_index=1, utility=6, values=[1, 2, 3, 4, 5, 6, 7, 8], branch_factor=2)
 
     def test_decision5(self):
         """
@@ -112,11 +115,58 @@ class Test(TestCase):
          3    4    8    7      max
         1 3  2 4  5 8  7 6     min
         """
-        self._test_decision(child_index=1, utility=7, values=[1, 3, 2, 4, 5, 8, 7, 6], branching=2)
+        self._test_decision(child_index=1, utility=7, values=[1, 3, 2, 4, 5, 8, 7, 6], branch_factor=2)
 
-    def _test_decision(self, child_index, utility, values, branching):
-        state = Node.from_list(values, branching)
+    def test_pruning1(self):
+        """
+                 3             max
+            1         3        min
+         1    2    3    4      max
+        """
+        self._test_pruning(expected=[1, 2, 3, 4], values=[1, 2, 3, 4], branch_factor=2)
+
+    def test_pruning2(self):
+        """
+                 3             max
+            3         1        min
+         4    3    2    1      max
+        """
+        self._test_pruning(expected=[4, 3, 2], values=[4, 3, 2, 1], branch_factor=2)
+
+    def test_pruning3(self):
+        """
+                      4                 max
+                 2         4            min
+            2         4         6       max
+         1    2    3    4    5    6     min
+        """
+        self._test_pruning(expected=[1, 2, 3, 4, 5, 6], values=[1, 2, 3, 4, 5, 6], branch_factor=3)
+
+    def test_pruning4(self):
+        """
+                      4                 max
+                 4         2            min
+            6         4         2       max
+         6    5    4    3    2    1     min
+        """
+        self._test_pruning(expected=[6, 5, 4, 2], values=[6, 5, 4, 3, 2, 1], branch_factor=3)
+
+    def _test_decision(self, child_index, utility, values, branch_factor):
+        state = Node.from_list(values, branch_factor)
         minimax = create_minimax(state)
         actual_child, actual_utility = minimax.decision()
         self.assertEqual(utility, actual_utility)
         self.assertEqual(state.children[child_index], actual_child)
+
+    def _test_pruning(self, expected, values, branch_factor):
+        evaluated_states = []
+
+        def eval_func(state):
+            ans = eval(state)
+            evaluated_states.append(ans)
+            return ans
+
+        state = Node.from_list(values, branch_factor=branch_factor)
+        minimax = create_minimax(state, eval_func=eval_func)
+        minimax.decision()
+        self.assertEqual(expected, evaluated_states)
